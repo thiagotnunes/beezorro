dcd1bb51063fbb0f549bbdf67ef731b5c0344816
Thiago Nunes <thiagotnunes@gmail.com>
2012-08-19 23:25:50 -0300

    Requiring all the files under the lib directory.

--- application.rb
+++ application.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+Dir[File.join(File.dirname(__FILE__), 'lib', '**', '*.rb')].each do |f|
+  require f
+end
 
 get '/' do
   File.read(File.join('public', 'index.html'))
