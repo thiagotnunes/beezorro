5a2889a3435fccba0ec6f6b96e508714013843b6
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2012-11-07 13:11:47 -0200

    grunt

---
+++ grunt.js
@@ -0,0 +1,48 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    lint: {
+      files: ['grunt.js', 'public/javascript/**/*.js', 'spec/**/*.js']
+    },
+    concat: {
+      dist: {
+        src: ['public/javascript'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<config:concat.dist.dest>'],
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit concat min'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {}
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint concat min');
+
+};
